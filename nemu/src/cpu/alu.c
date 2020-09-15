9914c7c27625244d69dccd085976d48ee9be3065
tracer-pa-public <tracer@njuics.org>
2020-09-15 14:24:31 +0800

    >  clean 202220016 pa202220016 Linux a74eeeb968f6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux  14:24:31 up 8 days,  5:46,  1 user,  load average: 2.25, 2.19, 2.16 7bc66e43c28c0be7329c8652e69539e4759cf34b

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -1,14 +1,73 @@
 #include "cpu/cpu.h"
 
+#ifndef NEMU_REF_ALU
+
+#define cutx(x, size) ((~((0xffffffff << ((size) - 1)) << 1)) & (x))	// return the lower <size> bits of x.
+#define signx(x, size) (!!((1u << ((size) - 1)) & (x)))	// return the sign bit of x, a integer of <size> bits length.
+
+inline void alu_set_SF_ZF_PF(uint32_t res, size_t data_size)
+{
+	cpu.eflags.SF = signx(res, data_size);
+	cpu.eflags.ZF = cutx(res, data_size) == 0;
+	
+	uint8_t pf_t = 1;
+	int i;
+	for(i=0; i<8; ++i) pf_t ^= (res>>i)&1;
+	cpu.eflags.PF = pf_t;
+}
+
+inline void alu_set_ADD_OF(uint32_t res, uint32_t src, uint32_t dest, size_t data_size)
+{
+	cpu.eflags.OF = signx(src, data_size) == signx(dest, data_size) && signx(res, data_size) != signx(src, data_size);
+}
+
+inline void alu_set_ADD_CF(uint32_t res, uint32_t src, size_t data_size)
+{
+	cpu.eflags.CF = cutx(res, data_size) < cutx(src, data_size);
+}
+
+inline void alu_set_ADC_OF(uint32_t res, uint32_t src, uint32_t dest, size_t data_size)
+{
+	cpu.eflags.OF = signx(src, data_size) == signx(dest, data_size) && signx(res, data_size) != signx(src, data_size);
+}
+
+inline void alu_set_ADC_CF(uint32_t res, uint32_t src, size_t data_size)
+{
+	cpu.eflags.CF = cutx(res, data_size) < cutx(src, data_size) || (cutx(res, data_size) == cutx(src, data_size) && cpu.eflags.CF == 1);
+}
+
+inline void alu_set_SUB_OF(uint32_t res, uint32_t src, uint32_t dest, size_t data_size)
+{
+	cpu.eflags.OF = signx(src, data_size) != signx(dest, data_size) && signx(res, data_size) != signx(dest, data_size);
+}
+
+inline void alu_set_SUB_CF(uint32_t src, uint32_t dest, size_t data_size)
+{
+	cpu.eflags.CF = cutx(dest, data_size) < cutx(src, data_size);
+}
+
+inline void alu_set_SBB_OF(uint32_t res, uint32_t src, uint32_t dest, size_t data_size)
+{
+	cpu.eflags.OF = signx(src, data_size) != signx(dest, data_size) && signx(res, data_size) != signx(dest, data_size);
+}
+
+inline void alu_set_SBB_CF(uint32_t src, uint32_t dest, size_t data_size)
+{
+	cpu.eflags.CF = cutx(dest, data_size) < cutx(src, data_size) || (cutx(dest, data_size) == cutx(src, data_size) && cpu.eflags.CF == 1);
+}
+
+#endif
+
 uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
 {
 #ifdef NEMU_REF_ALU
 	return __ref_alu_add(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	fflush(stdout);
-	assert(0);
-	return 0;
+	uint32_t res = src + dest;
+	alu_set_ADD_OF(res, src, dest, data_size);
+	alu_set_SF_ZF_PF(res, data_size);
+	alu_set_ADD_CF(res, src, data_size);
+	return cutx(res, data_size);
 #endif
 }
 
@@ -17,10 +76,11 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_adc(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	fflush(stdout);
-	assert(0);
-	return 0;
+	uint32_t res = src + dest + cpu.eflags.CF;
+	alu_set_ADC_OF(res, src, dest, data_size);
+	alu_set_SF_ZF_PF(res, data_size);
+	alu_set_ADC_CF(res, src, data_size);
+	return cutx(res, data_size);
 #endif
 }
 
@@ -29,10 +89,11 @@ uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_sub(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	fflush(stdout);
-	assert(0);
-	return 0;
+	uint32_t res = dest - src;
+	alu_set_SUB_OF(res, src, dest, data_size);
+	alu_set_SF_ZF_PF(res, data_size);
+	alu_set_SUB_CF(src, dest, data_size);
+	return cutx(res, data_size);
 #endif
 }
 
@@ -41,10 +102,11 @@ uint32_t alu_sbb(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_sbb(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	fflush(stdout);
-	assert(0);
-	return 0;
+	uint32_t res = dest - src - cpu.eflags.CF;
+	alu_set_SBB_OF(res, src, dest, data_size);
+	al_set_SF_ZF_PF(res, data_size);
+	alu_set_SBB_CF(src, dest, data_size);
+	return cutx(res, data_size);
 #endif
 }
 
