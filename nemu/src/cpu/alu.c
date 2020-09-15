32c7e1ad22df18c369abbfac5ad5f0e1fdcdb2ff
tracer-pa-public <tracer@njuics.org>
2020-09-15 20:42:54 +0800

    >  compile nemu testcase kernel 202220016 pa202220016 Linux a74eeeb968f6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux  20:42:54 up 8 days, 12:05,  1 user,  load average: 2.89, 2.27, 2.13 c4141e451d5828fd285d5703263e4e65123ad700

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -236,20 +236,11 @@ uint32_t alu_shl(uint32_t src, uint32_t dest, size_t data_size)
 #else
 	src &= 0x1f;
 	uint32_t msk = mask(data_size);
-	uint32_t res;
-	switch(data_size / 8)
-	{
-		case 1 : res = ((uint32_t)(uint8_t)dest) << src; break;
-		case 2 : res = ((uint32_t)(uint16_t)dest) << src; break;
-		case 4 : res = ((uint32_t)dest) << src; break;
-		default : assert(0);
-	}
-	res = (res & msk) | (dest & (~msk));
-	//uint32_t res = (((dest & msk) << src) & msk) | ((~msk) & dest);
+	uint32_t res = ((dest & msk) << src) & msk;
 	//cpu.eflags.OF is ignored.
 	alu_set_SF_ZF_PF(res, data_size);
-	cpu.eflags.CF = (data_size < src) ? 0 : signx(dest, data_size - src + 1); //!!(bitMask(data_size - src + 1) & dest);
-	return cutx(res, data_size);
+	cpu.eflags.CF = (data_size < src) ? 0 : !!(bitMask(data_size - src + 1) & dest);
+	return res;
 #endif
 }
 
@@ -260,7 +251,7 @@ uint32_t alu_shr(uint32_t src, uint32_t dest, size_t data_size)
 #else
 	src &= 0x1f;
 	uint32_t msk = mask(data_size);
-	uint32_t res = (((dest & msk) >> src) & msk) | ((~msk) & dest);
+	uint32_t res = ((dest & msk) >> src) & msk;
 	//cpu.eflags.OF is ignored.
 	alu_set_SF_ZF_PF(res, data_size);
 	cpu.eflags.CF = (data_size < src) ? 0 : !!(bitMask(src) & dest);
@@ -284,7 +275,7 @@ uint32_t alu_sar(uint32_t src, uint32_t dest, size_t data_size)
 		case 4 : res = ((int32_t)dest) >> src; break;
 		default : assert(0);
 	}
-	res = (res & msk) | (dest & (~msk));
+	res &= msk;
 	//cpu.eflags.OF is ignored.
 	alu_set_SF_ZF_PF(res, data_size);
 	cpu.eflags.CF = (data_size < src) ? signx(dest, data_size) : !!(bitMask(src) & dest);
