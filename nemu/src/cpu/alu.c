baf5e2eed825873efb943583da79dee4214c2070
tracer-pa-public <tracer@njuics.org>
2020-09-16 23:47:46 +0800

    > compile nemu testcase kernel > id 202220016 > user pa202220016 > uname Linux a74eeeb968f6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 23:47:46 up 9 days, 15:10,  1 user,  load average: 1.95, 2.13, 2.25 > unitime 1600271266477 > 281458cbdc059fc6884922d5b19e7f0de629675c

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -254,6 +254,7 @@ uint32_t alu_sar(uint32_t src, uint32_t dest, size_t data_size)
 	src &= 0x1f;
 	uint32_t msk = mask(data_size);
 #if 0
+    // Another implementation
 	uint32_t res = (((dest & msk) >> src) & msk) | (signx(dest, data_size) == 0 ? 0 : (msk ^ (data_size < src ? 0 : mask(data_size - src))));
 #else
 	uint32_t res;
