c7c686cf8e142dd49986a729409ad8563b02f408
tracer-pa-public <tracer@njuics.org>
2020-10-26 21:56:49 +0800

    > compile nemu testcase kernel > id 202220016 > user pa202220016 > uname Linux a74eeeb968f6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 21:56:49 up 49 days, 13:19,  2 users,  load average: 2.62, 2.41, 2.31 > unitime 1603720609433 > d61e6440466164733e2d8a6c2cbd6be71778c2d0

--- nemu/src/cpu/instr/leave.c
+++ nemu/src/cpu/instr/leave.c
@@ -2,3 +2,27 @@
 /*
 Put the implementations of `leave' instructions here.
 */
+
+make_instr_func(leave)
+{
+    cpu.esp = cpu.ebp;
+    
+    OPERAND fetcher, ebp;
+    fetcher.type = OPR_MEM;
+    fetcher.addr = cpu.esp;
+    fetcher.sreg = SREG_SS;
+    fetcher.data_size = data_size;
+    operand_read(&fetcher);
+    
+    ebp.type = OPR_REG;
+    ebp.addr = REG_EBP;
+    ebp.data_size = data_size;
+    ebp.val = fetcher.val;
+    operand_write(&ebp);
+    
+    cpu.esp += data_size / 8;
+    
+    print_asm_0("leave", "", 1);
+    
+    return 1;
+}
