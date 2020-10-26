f627407d7dffba0551fc1348f46ca4189018c49f
tracer-pa-public <tracer@njuics.org>
2020-10-26 21:10:10 +0800

    > compile nemu testcase kernel > id 202220016 > user pa202220016 > uname Linux a74eeeb968f6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 21:10:10 up 49 days, 12:32,  2 users,  load average: 2.09, 2.02, 2.09 > unitime 1603717810914 > 83d39fe762d4e518827561dde5b153f0b1a3d292

--- nemu/src/cpu/instr/pop.c
+++ nemu/src/cpu/instr/pop.c
@@ -2,3 +2,19 @@
 /*
 Put the implementations of `pop' instructions here.
 */
+
+#define dest_prep               \
+    opr_dest.type = OPR_MEM;    \
+    opr_dest.addr = cpu.esp;    \
+    opr_dest.sreg = SREG_SS;
+
+static void instr_execute_1op(){
+    dest_prep
+    operand_read(&opr_dest);
+    cpu.esp += opr_src.data_size / 8;
+    opr_src.val = opr_dest.val;
+    operand_write(&opr_src);
+}
+
+make_instr_impl_1op(pop, r, v)
+make_instr_impl_1op(pop, rm, v)
