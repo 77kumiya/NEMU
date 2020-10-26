6d2696f25ae09e197fc07311b42f3dfbb735aa28
tracer-pa-public <tracer@njuics.org>
2020-10-26 16:00:13 +0800

    > compile nemu testcase kernel > id 202220016 > user pa202220016 > uname Linux a74eeeb968f6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:00:13 up 49 days,  7:22,  1 user,  load average: 2.30, 2.28, 2.19 > unitime 1603699213033 > a0d906f9a6262409ab918736f3414ce79337443e

--- nemu/src/cpu/instr/lea.c
+++ nemu/src/cpu/instr/lea.c
@@ -2,3 +2,21 @@
 /*
 Put the implementations of `lea' instructions here.
 */
+
+static void instr_execute_2op()
+{
+        opr_dest.val = opr_src.addr;
+        operand_write(&opr_dest);
+}
+
+make_instr_func(lea)
+{
+        int len = 1;
+		opr_dest.data_size = data_size;
+		decode_operand_rm2r
+		
+		print_asm_2(lea, "", len, &opr_src, &opr_dest);
+		
+		instr_execute_2op();
+		return len; 
+}
