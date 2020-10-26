744425ef714dad7358c03e57166521c7bded0527
tracer-pa-public <tracer@njuics.org>
2020-10-26 09:35:58 +0800

    > compile nemu testcase kernel > id 202220016 > user pa202220016 > uname Linux a74eeeb968f6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 09:35:58 up 49 days, 58 min,  1 user,  load average: 2.84, 2.51, 2.30 > unitime 1603676158465 > be6eb93b96c5cc2682a14408a0448a34e98a5f09

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -2,3 +2,18 @@
 /*
 Put the implementations of `push' instructions here.
 */
+
+#define dest_prep               \
+    opr_dest.type = OPR_MEM;    \
+    opr_dest.addr = cpu.esp;    \
+    opr_dest.sreg = SREG_SS;    \
+    opr_dest.val = opr_src.val;
+
+static void instr_execute_1op(){
+    operand_read(&opr_src);
+    cpu.esp -= opr_src.data_size / 8;
+    dest_prep
+    operand_write(&opr_dest);
+}
+
+make_instr_impl_1op(push, r, v)
