f4ab3588fd143752168d6c03818ee037d0136103
tracer-pa-public <tracer@njuics.org>
2020-10-26 16:28:23 +0800

    > compile nemu testcase kernel > id 202220016 > user pa202220016 > uname Linux a74eeeb968f6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:28:23 up 49 days,  7:50,  1 user,  load average: 2.31, 2.33, 2.23 > unitime 1603700903727 > 2c4a896fe67f8e8793cc225c7da3da9f18e776a

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -2,3 +2,15 @@
 /*
 Put the implementations of `inc' instructions here.
 */
+
+static void instr_execute_1op()
+{
+    operand_read(&opr_src);
+    bool original_CF = cpu.eflags.CF;  //inc does not change the carry flag, so keep the original CF
+    opr_src.val = alu_add(1, opr_src.val, opr_src.data_size);
+    operand_write(&opr_src);
+    cpu.eflags.CF = original_CF;
+}
+
+make_instr_impl_1op(inc, rm, v)
+make_instr_impl_1op(inc, rm, b)
