d51fb95c12e50a11175b4cb68fa236d203e2f3f9
tracer-pa-public <tracer@njuics.org>
2020-12-22 16:58:43 +0800

    > compile nemu testcase kernel > id 202220016 > user pa202220016 > uname Linux a74eeeb968f6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:58:43 up 106 days,  8:21,  4 users,  load average: 2.07, 2.13, 2.17 > unitime 1608627523438 > 1e28175b415b7352fcf891092e558d273cf9eabf

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -13,8 +13,8 @@ void load_sreg(uint8_t sreg)
 	/* TODO: load the invisibile part of the segment register 'sreg' by reading the GDT.
 	 * The visible part of 'sreg' should be assigned by mov or ljmp already.
 	 */
-	assert(sreg == 1);
 	uint32_t desc_linear_addr = cpu.gdtr.base + (sreg << 3);
+	assert(desc_linear_addr == 0x0003003c);
 	SegDesc sreg_desc;
     sreg_desc.val[0] = laddr_read(desc_linear_addr, 4);
     sreg_desc.val[1] = laddr_read(desc_linear_addr + 4, 4);
