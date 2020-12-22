c7b78970da1f5daf526f6ca022cb753c0d052403
tracer-pa-public <tracer@njuics.org>
2020-12-22 15:45:41 +0800

    > compile nemu testcase kernel > id 202220016 > user pa202220016 > uname Linux a74eeeb968f6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 15:45:41 up 106 days,  7:08,  4 users,  load average: 2.17, 2.32, 2.29 > unitime 1608623140998 > 9d277aed148c49d054c68fbbf110dd025f351511

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -22,5 +22,5 @@ void load_sreg(uint8_t sreg)
     cpu.segReg[sreg].type = sreg_desc.type;
     cpu.segReg[sreg].privilege_level = sreg_desc.privilege_level;
     cpu.segReg[sreg].soft_use = sreg_desc.soft_use;
-    assert(cpu.segReg[sreg].base == 0x0 && cpu.segReg[sreg].limit == 0xfffff && sreg_desc.granularity == 1);
+    assert(cpu.segReg[sreg].base == 0x0 && cpu.segReg[sreg].limit == 0x000fffff && sreg_desc.granularity == 1);
 }
