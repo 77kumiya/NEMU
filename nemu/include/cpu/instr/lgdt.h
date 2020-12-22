0ea94a424142d4c476a161750dede888dbf1be03
tracer-pa-public <tracer@njuics.org>
2020-12-22 11:59:53 +0800

    > compile nemu testcase kernel > id 202220016 > user pa202220016 > uname Linux a74eeeb968f6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:59:53 up 106 days,  3:22,  4 users,  load average: 2.17, 2.26, 2.30 > unitime 1608609593208 > 558ced8863cff47b29b99b46b2adef771ea34ae4

--- nemu/include/cpu/instr/lgdt.h
+++ nemu/include/cpu/instr/lgdt.h
@@ -4,6 +4,6 @@
 Put the declarations of `lgdt' instructions here.
 */
 
-make_instr_func(lgdt_m_v);
+make_instr_func(lgdt);
 
 #endif
--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -3,9 +3,16 @@
 Put the implementations of `lgdt' instructions here.
 */
 
-static void instr_execute_1op(){
-    cpu.gdtr.limit = paddr_read(opr_src.addr, 2);
-    cpu.gdtr.base = paddr_read(opr_src.addr + 2, 4);
+make_instr_func(lgdt){
+    int len = 2;
+    uint32_t gdt_addr = instr_fetch(eip + 2, 4);
+    cpu.gdtr.limit = paddr_read(gdt_addr, 2);
+    cpu.gdtr.base = paddr_read(gdt_addr + 2, 4);
+    len += 4;
+    opr_src.type = OPR_MEM;
+    opr_src.addr = gdt_addr;
+    opr_src.data_size = 32;
+    opr_src.sreg = SREG_DS;
+    print_asm_1("lgdt", "", len, &opr_src);
+    return len;
 }
-
-make_instr_impl_1op(lgdt, m, v)
