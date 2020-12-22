b4b997d97763622c62805906e3254a34b1ae3bde
tracer-pa-public <tracer@njuics.org>
2020-12-22 16:35:04 +0800

    > compile nemu testcase kernel > id 202220016 > user pa202220016 > uname Linux a74eeeb968f6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:35:04 up 106 days,  7:57,  4 users,  load average: 2.21, 2.24, 2.28 > unitime 1608626104454 > a42c322ce034ff045aaac4cfcabdf66e4033dbba

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -6,12 +6,12 @@ Put the implementations of `lgdt' instructions here.
 static void instr_execute_1op(){
     opr_src.data_size = 16;
     operand_read(&opr_src);
-    cpu.ldgt.limit = opr_src.val;
+    cpu.gdtr.limit = opr_src.val;
     
     opr_src.data_size = 32;
     opr_src.addr += 2;
     operand_read(&opr_src);
-    cpu.ldgt.base = opr_src.val;
+    cpu.gdtr.base = opr_src.val;
 }
 
 make_instr_impl_1op(lgdt, rm, v)
