91e8013cf00d3204ad5ae6e6fc953350c6de8176
tracer-pa-public <tracer@njuics.org>
2020-12-22 11:30:12 +0800

    > clean > id 202220016 > user pa202220016 > uname Linux a74eeeb968f6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:30:12 up 106 days,  2:52,  4 users,  load average: 2.29, 2.23, 2.23 > unitime > 50df1ff96b45f30ddf6ecfc05bb4a35aa6eeddc5

--- nemu/include/cpu/instr/jmp.h
+++ nemu/include/cpu/instr/jmp.h
@@ -4,5 +4,6 @@
 make_instr_func(jmp_near);
 make_instr_func(jmp_short);
 make_instr_func(jmp_rm_v);
+make_instr_func(ljmp);
 
 #endif
--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -56,14 +56,22 @@ make_instr_func(jmp_rm_v)
 make_instr_func(ljmp)
 {
         int len = 1;
-        decode_data_size_v
+        uint32_t eaddr = instr_fetch(eip + 1, data_size >> 3);
+        uint16_t selector = instr_fetch(eip + 1 + (data_size >> 3), 2);
+        cpu.cs.val = selector;
+        load_sreg((uint8_t)cpu.cs.index);
         
+        // print asm
+        len += 2 + (data_size >> 3);
+        opr_src.type = OPR_IMM;
+        opr_src.data_size = 16;
+        opr_src.val = selector;
+        opr_dest.type = OPR_IMM;
+        opr_dest.data_size = data_size;
+        opr_dest.val = eaddr;
+        print_asm_2("ljmp", "", len, &opr_src, &opr_dest);
 
-        operand_read(&opr_src);
-        
-        print_asm_1("jmp", "", len, &opr_src);
-
-        cpu.eip = opr_src.val;
+        cpu.eip = eaddr;
 
         return 0;
 }
