947a3f0c0548f6266f0251f9ffedb7d0ccdaf09b
tracer-pa-public <tracer@njuics.org>
2021-01-21 18:48:48 +0800

    > compile nemu testcase kernel > id 202220016 > user icspa > uname Linux icspa 4.19.0-13-686 #1 SMP Debian 4.19.160-2 (2020-11-28) i686 GNU/Linux > uptime 18:48:48 up  2:45,  1 user,  load average: 0.15, 0.10, 0.03 > unitime 1611226128312 > d038ff90b5e03ba043d51ede90b7d40d4ccf492

--- nemu/src/cpu/instr/int_.c
+++ nemu/src/cpu/instr/int_.c
@@ -8,10 +8,11 @@ Special note for `int': please use the instruction name `int_' instead of `int'.
 */
 
 make_instr_func(int_){
+	int len = 1;
 	decode_operand_i
 	decode_data_size_b
 	operand_read(&opr_src);
-	print_asm_1("int", "", 2, &opr_src);
+	print_asm_1("int", "", len, &opr_src);
 	raise_sw_intr(opr_src.val);
 	return 0;
 }
