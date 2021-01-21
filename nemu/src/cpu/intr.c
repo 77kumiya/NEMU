e33cc21b93676f1421cf3c834ef44f584442c4ec
tracer-pa-public <tracer@njuics.org>
2021-01-21 19:41:51 +0800

    > compile nemu testcase kernel > id 202220016 > user icspa > uname Linux icspa 4.19.0-13-686 #1 SMP Debian 4.19.160-2 (2020-11-28) i686 GNU/Linux > uptime 19:41:51 up  3:39,  1 user,  load average: 0.20, 0.08, 0.02 > unitime 1611229311461 > ea524d22ef5a2c7e8903d81e028defe4f44d9c5

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -2,12 +2,36 @@
 #include "cpu/instr.h"
 #include "memory/memory.h"
 
+static void push(uint32_t data, uint32_t size){
+	OPERAND dest;
+        cpu.esp -= 4;
+        dest.type = OPR_MEM;
+        dest.sreg = SREG_SS;
+        dest.data_size = size;
+        dest.addr = cpu.esp;
+        dest.val = data;
+        operand_write(&dest);
+}
+
 void raise_intr(uint8_t intr_no)
 {
 #ifdef IA32_INTR
-	printf("Please implement raise_intr()");
-	fflush(stdout);
-	assert(0);
+	// push eflags, cs and eip onto stack
+        push(cpu.eflags, 32);
+	push(cpu.cs.val, 32);
+	push(cpu.eip, 32);
+	// find the IDT entry
+	GateDesc *idt_entry = (GateDesc *)(cpu.idtr.base + (intr_no << 3));
+	assert(idt_entry->present == 1);
+	// clear IF if it is an interrupt
+	if(idt_entry->type == INTERRUPT_GATE_32){
+		cpu.eflags.IF = 0;
+	}
+	// set the cs:eip to the entry of the interrupt handler
+		// need to reload cs with load_sreg()
+	cpu.cs.val = idt_entry->segment;
+	load_sreg(idt_entry->segment >> 3);
+	cpu.eip = (idt_entry->offset_31_16 << 16) | idt_entry->offset_15_0;
 #endif
 }
 
