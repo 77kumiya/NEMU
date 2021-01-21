7599000baf0c9cf5f09c03026439f4547f9b4b97
tracer-pa-public <tracer@njuics.org>
2021-01-21 20:23:35 +0800

    > compile nemu testcase kernel > id 202220016 > user icspa > uname Linux icspa 4.19.0-13-686 #1 SMP Debian 4.19.160-2 (2020-11-28) i686 GNU/Linux > uptime 20:23:35 up  4:20,  1 user,  load average: 0.15, 0.08, 0.01 > unitime 1611231815869 > 26134b95e4bdb7a4285187f4b91937d26030cc89

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -21,18 +21,20 @@ void raise_intr(uint8_t intr_no)
 	push(cpu.cs.val, 32);
 	push(cpu.eip, 32);
 	// find the IDT entry
-	GateDesc *idt_entry = (GateDesc *)(cpu.idtr.base + (intr_no << 3));
-	assert(idt_entry->present == 1);
-	assert(0);
+	laddr_t idt_entry_addr = cpu.idtr.base + (intr_no << 3);
+	GateDesc idt_entry;
+       	idt_entry.val[0] = laddr_read(idt_entry_addr, 4);
+	idt_entry.val[1] = laddr_read(idt_entry_addr + 4, 4);
+	assert(idt_entry.present == 1);
 	// clear IF if it is an interrupt
-	if(idt_entry->type == 0xE){
+	if(idt_entry.type == 0xE){
 		cpu.eflags.IF = 0;
 	}
 	// set the cs:eip to the entry of the interrupt handler
 		// need to reload cs with load_sreg()
-	cpu.cs.val = idt_entry->selector;
-	load_sreg(idt_entry->selector >> 3);
-	cpu.eip = (idt_entry->offset_31_16 << 16) | idt_entry->offset_15_0;
+	cpu.cs.val = idt_entry.selector;
+	load_sreg(idt_entry.selector >> 3);
+	cpu.eip = (idt_entry.offset_31_16 << 16) | idt_entry.offset_15_0;
 #endif
 }
 
