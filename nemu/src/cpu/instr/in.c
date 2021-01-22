6b81920877186e6f2b241b09247279426783d1e9
tracer-pa-public <tracer@njuics.org>
2021-01-22 21:32:07 +0800

    > compile nemu testcase kernel > id 202220016 > user pa202220016 > uname Linux icspa 4.19.0-13-686 #1 SMP Debian 4.19.160-2 (2020-11-28) i686 GNU/Linux > uptime 21:32:07 up 36 min,  1 user,  load average: 0.05, 0.09, 0.09 > unitime 1611322327483 > ba2110c1a249a63b179c73cd61761b9c5a7ece85

--- nemu/src/cpu/instr/in.c
+++ nemu/src/cpu/instr/in.c
@@ -5,13 +5,17 @@ Put the implementations of `in' instructions here.
 */
 
 make_instr_func(in_b){
-	cpu.gpr[REG_AL]._8[0] = pio_read(cpu.gpr[REG_DX]._16, 1);
+	cpu.gpr[REG_AL]._8[0] = (uint8_t)pio_read(cpu.gpr[REG_DX]._16, 1);
 	return 1;
 }
 
 make_instr_func(in_v){
 	uint32_t len = data_size >> 3;
-	(len == 2 ? cpu.gpr[REG_AX]._16 : cpu.eax) = pio_read(cpu.gpr[REG_DX]._16, len);
+	if(len == 2){
+		cpu.gpr[REG_AX]._16 = pio_read(cpu.gpr[REG_DX]._16, len);
+	}else{
+		cpu.eax = pio_read(cpu.gpr[REG_DX]._16, len);
+	}
 	return 1;
 }
 
