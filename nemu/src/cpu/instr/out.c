3a1854f3d42b2fcc2cad652025bcca2509b98c5d
tracer-pa-public <tracer@njuics.org>
2021-01-22 21:38:22 +0800

    > compile nemu testcase kernel > id 202220016 > user pa202220016 > uname Linux icspa 4.19.0-13-686 #1 SMP Debian 4.19.160-2 (2020-11-28) i686 GNU/Linux > uptime 21:38:22 up 43 min,  1 user,  load average: 0.00, 0.07, 0.08 > unitime 1611322702830 > c0c620b231a934a79cb9d318b5b98d3e3557c9d6

--- nemu/src/cpu/instr/out.c
+++ nemu/src/cpu/instr/out.c
@@ -3,3 +3,19 @@
 /*
 Put the implementations of `out' instructions here.
 */
+
+make_instr_func(out_b){
+	pio_write(cpu.gpr[REG_DX]._16, 1, cpu.gpr[REG_AL]._8[0]);
+	return 1;
+}
+
+make_instr_func(out_v){
+	uint32_t len = data_size >> 3;
+	if(len == 2){
+		pio_write(cpu.gpr[REG_DX]._16, len, cpu.gpr[REG_AX]._16);
+	}else{
+		pio_write(cpu.gpr[REG_DX]._16, len, cpu.eax)
+	}
+	return 1;
+}
+
